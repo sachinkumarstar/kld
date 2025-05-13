386abb48006f17493c1fa09ac73f346ad193d817
sachinkumarstar <sachinkumarstar007@gmail.com>
2025-05-13 10:32:06 +0530

    New Table Added

--- mo.php
+++ mo.php
@@ -1,12 +1,55 @@
-<?php
-echo "<h1>Time Table of 5 Subject</h1>";
-echo "<table border='1'>";
-echo "<tr><th>Day</th><th>8:00-9:00</th><th>9:00-10:00</th><th>10:00-11:00</th><th>11:00-12:00</th><th>12:00-1:00</th></tr>";
-echo "<tr><td>Monday</td><td>Math</td><td>Science</td><td>English</td><td>Hindi</td><td>Computer</td></tr>";
-echo "<tr><td>Tuesday</td><td>English</td><td>Math</td><td>Science</td><td>Hindi</td><td>Social</td></tr>";
-echo "<tr><td>Wednesday</td><td>Science</td><td>English</td><td>Math</td><td>Social</td><td>Computer</td></tr>";
-echo "<tr><td>Thursday</td><td>Hindi</td><td>Social</td><td>English</td><td>Math</td><td>Science</td></tr>";
-echo "<tr><td>Friday</td><td>Computer</td><td>Hindi</td><td>Social</td><td>Science</td><td>English</td></tr>";
-echo "</table>";
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testing</title>
+</head>
+<body>
+    <h1>School Subject Time Table</h1>
+    <table border="1">
+        <tr>
+            <th>Day</th>
+            <th>8:00-9:00</th>
+            <th>9:00-10:00</th>
+            <th>10:00-11:00</th>
+            <th>11:00-12:00</th>
+        </tr>
+        <tr>
+            <td>Monday</td>
+            <td>Math</td>
+            <td>English</td>
+            <td>Science</td>
+            <td>Social Study</td>
+        </tr>
+        <tr>
+            <td>Tuesday</td>
+            <td>Computer</td>
+            <td>Hindi</td>
+            <td>Physics</td>
+            <td>Chemistry</td>
+        </tr>
+        <tr>
+            <td>Wednesday</td>
+            <td>Sanskrit</td>
+            <td>English</td>
+            <td>Math</td>
+            <td>Biological Science</td>
+        </tr>
+        <tr>
+            <td>Thursday</td>
+            <td>History</td>
+            <td>Geography</td>
+            <td>Economics</td>
+            <td>Political Science</td>
+        </tr>
+        <tr>
+            <td>Friday</td>
+            <td>PT</td>
+            <td>Art</td>
+            <td>Music</td>
+            <td>Computer</td>
+        </tr>
+    </table>
+</body>
+</html>
