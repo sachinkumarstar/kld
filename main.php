6bee46c059ea2df9da13aa14f9d69e39b648ee11
sachinkumarstar <sachinkumarstar007@gmail.com>
2025-05-13 10:27:50 +0530

    New Pattern Added

--- main.php
+++ main.php
@@ -1,1 +1,12 @@
-<?php echo "Hello i'm Sachin Kumarhdght heher getg !";?>
+<?php echo "Hello i'm Sachin Kumarhdght heher getg !";
+$rows = 5;
+for ($i = 1; $i <= $rows; $i++) {
+    for ($j = $i; $j < $rows; $j++) {
+        echo "&nbsp;&nbsp;";
+    }
+    for ($j = 1; $j <= (2 * $i - 1); $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
